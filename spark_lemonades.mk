746260b8303b2e22bc7e87e4caaf025634254e9c
sodalime0509 <76208711+sodalime0509@users.noreply.github.com>
2023-06-15 19:32:06 +0530

    Update and rename lineage_lemonades.mk to spark_lemonades.mk

---
+++ spark_lemonades.mk
@@ -0,0 +1,52 @@
+#
+# Copyright (C) 2018 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit from lemonades device
+$(call inherit-product, device/oneplus/lemonades/device.mk)
+
+# Inherit some common Evolution X stuff.
+$(call inherit-product, vendor/spark/config/common_full_phone.mk)
+
+EXTRA_UDFPS_ANIMATIONS := true
+TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_SUPPORTS_QUICK_TAP := true
+
+PRODUCT_NAME := spark_lemonades
+PRODUCT_DEVICE := lemonades
+PRODUCT_MANUFACTURER := OnePlus
+PRODUCT_BRAND := OnePlus
+PRODUCT_MODEL := LE2101
+
+# BananaDroid
+
+WITH_GAPPS := true
+TARGET_ENABLE_BLUR := true
+TARGET_SUPPORTS_GOOGLE_RECORDER := true
+TARGER_SUPPORTS_NEXT_GEN_ASSISTANT := true
+TARGET_EXCLUDE_MATLOG := true
+TARGET_SUPPORTS_QUICK_TAP := true
+PRODUCT_SYSTEM_NAME := OnePlus9R
+PRODUCT_SYSTEM_DEVICE := OnePlus9R
+
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="OnePlus9R_IND-user 13 RKQ1.211119.001 R.eea0db-2-1 release-keys" \
+    TARGET_DEVICE=$(PRODUCT_SYSTEM_DEVICE) \
+    TARGET_PRODUCT=$(PRODUCT_SYSTEM_NAME)
+
+BUILD_FINGERPRINT := OnePlus/OnePlus9R_IND/OnePlus9R:13/RKQ1.211119.001/R.eea0db-2-1:user/release-keys
+
+
+# UDFPS ICONS/ANIMATIONS
+TARGET_HAS_UDFPS := true
+
+
+
+
+
