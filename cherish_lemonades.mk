c309c5070c97275f9ea542a22a5aa19e12fdd6a8
sodalime0509 <76208711+sodalime0509@users.noreply.github.com>
2023-06-18 15:52:36 +0530

    Update and rename spark_lemonades.mk to cherish_lemonades.mk

--- cherish_lemonades.mk
+++ cherish_lemonades.mk
@@ -11,26 +11,28 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/oneplus/lemonades/device.mk)
 
 # Inherit some common Evolution X stuff.
-$(call inherit-product, vendor/spark/config/common_full_phone.mk)
+$(call inherit-product, vendor/cherish/config/common_full_phone.mk)
 
+# FOD animations
 EXTRA_UDFPS_ANIMATIONS := true
-TARGET_BOOT_ANIMATION_RES := 1080
-TARGET_SUPPORTS_QUICK_TAP := true
 
-PRODUCT_NAME := spark_lemonades
+PRODUCT_NAME := cherish_lemonades
 PRODUCT_DEVICE := lemonades
 PRODUCT_MANUFACTURER := OnePlus
 PRODUCT_BRAND := OnePlus
 PRODUCT_MODEL := LE2101
 
-# BananaDroid
-
+# Rom Specific Flags
+TARGET_INCLUDE_WIFI_EXT := true
+TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_FACE_UNLOCK_SUPPORTED := true
+TARGET_INCLUDE_LIVE_WALLPAPERS := true
+TARGET_SUPPORTS_QUICK_TAP  := true
 WITH_GAPPS := true
-TARGET_ENABLE_BLUR := true
+TARGET_USES_BLUR := true
 TARGET_SUPPORTS_GOOGLE_RECORDER := true
 TARGER_SUPPORTS_NEXT_GEN_ASSISTANT := true
 TARGET_EXCLUDE_MATLOG := true
-TARGET_SUPPORTS_QUICK_TAP := true
 PRODUCT_SYSTEM_NAME := OnePlus9R
 PRODUCT_SYSTEM_DEVICE := OnePlus9R
 
@@ -43,8 +45,7 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
 BUILD_FINGERPRINT := OnePlus/OnePlus9R_IND/OnePlus9R:13/RKQ1.211119.001/R.eea0db-2-1:user/release-keys
 
 
-# UDFPS ICONS/ANIMATIONS
-TARGET_HAS_UDFPS := true
+
 
 
 
